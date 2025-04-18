2ebe3e51b445281d99238d186b791aa8af1637b2
sudrain <densedra@gmail.com>
2025-04-18 16:34:54 +0500

    add login functions

--- app/models.py
+++ app/models.py
@@ -3,9 +3,10 @@ from typing import Optional
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, \
         unique=True)
@@ -37,3 +38,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
