85f654935eb8860416d3b963b08197349187da2f
sudrain <densedra@gmail.com>
2025-04-18 11:34:50 +0500

    add password check/add hash

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 from typing import Optional
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
@@ -14,6 +15,12 @@ class User(db.Model):
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
