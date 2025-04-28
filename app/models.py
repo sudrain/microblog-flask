5499d13d6481a82b25b8d740780da4d4fc491408
sudrain <densedra@gmail.com>
2025-04-28 12:07:02 +0500

    add new fields in user model

--- app/models.py
+++ app/models.py
@@ -13,8 +13,11 @@ class User(UserMixin, db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
